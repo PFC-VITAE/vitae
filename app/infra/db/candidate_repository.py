e69dc8b20af541068169bd89385a96433b84f9e0
PamellaAtanes <atanes.pamella@gmail.com>
2024-07-29 17:39:50 -0300

    save resume in minio

--- app/infra/db/candidate_repository.py
+++ app/infra/db/candidate_repository.py
@@ -1,14 +1,16 @@
 from core.interfaces.candidate_repository import ICandidateRepository
 from ..db.dto.candidate_dto import CandidateDTO
-from infra.db.connection import dgp_connection, db_connection
+from infra.db.connection import dgp_connection, db_connection, dl_connection
 from core.entities.candidate import Candidate
 from infra.helpers.dict_to_object import dict_to_object
+import json, os
 
 class CandidateRepository(ICandidateRepository):
 
-    def __init__(self, dgp_client=dgp_connection, db_client=db_connection):
+    def __init__(self, dgp_client=dgp_connection, db_client=db_connection, dl_client=dl_connection):
         self.dgp_client = dgp_client
         self.db_client = db_client
+        self.dl_client = dl_client
 
 
     def get_all_personal_info(self):
@@ -27,13 +29,18 @@ class CandidateRepository(ICandidateRepository):
     def insert_candidates(self, candidates_list):
         try:
             db = self.db_client["vitae"]
-            collection = db["consolidated_data"]
+            collection = db["candidates"]
 
-            candidates_dict_list = [candidate.to_dict() for candidate in candidates_list]
+            candidates_dict_list = [candidate.__dict__ for candidate in candidates_list]
 
-            data = collection.insert_many(candidates_dict_list)
+            for candidate in candidates_dict_list:
+                candidate_id = candidate.get('_id')
 
-            return data  
+                if candidate_id:
+                    collection.replace_one({'_id': candidate_id}, candidate, upsert=True)
+
+
+            return "Dados inseridos ou atualizados com sucesso"
               
         except Exception as e:
             raise ValueError("Erro ao inserir dados no banco") from e
@@ -42,12 +49,40 @@ class CandidateRepository(ICandidateRepository):
     def get_all_candidates(self):
         try:
             db = self.db_client["vitae"]
-            collection = db["consolidated_data"]
+            collection = db["candidates"]
             documents = list(collection.find())
 
-            candidates_data = [Candidate(dict_to_object(doc)) for doc in documents]
+            candidates_data = [(dict_to_object(doc)) for doc in documents]
 
             return candidates_data
         
         except Exception as e:
             raise ValueError("Erro ao buscar dados no banco") from e
+        
+
+    def insert_dl_resume(self, cpf, resume, file_extension):
+        if file_extension == 'xml':
+            folder = 'lattes'
+            file_content = resume
+        elif file_extension == 'json':
+            folder = 'dgp'
+            file_content = json.dumps([curso.__dict__ for curso in resume], ensure_ascii=False)
+        
+        if file_content:
+            file_name = f"./app/infra/storage/data/{cpf}.{file_extension}"
+            object_name = f"raw/{folder}/{cpf}.{file_extension}"
+
+            with open(file_name, 'w', encoding='utf-8') as file:
+                file.write(file_content)
+            
+            try:
+                self.dl_client.fput_object(
+                    "vitae",
+                    object_name,
+                    file_name,
+                    content_type=f'application/{file_extension}'
+                )
+            except ValueError as e:
+                print(f"ERROR: {e}")
+
+            os.remove(file_name)
