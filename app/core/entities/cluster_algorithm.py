08caa30896dd0e4c75f0ee7b012299e2072c3304
pampamella <atanes.pamella@gmail.com>
2024-09-12 11:59:18 -0300

    testing cluster algorithm

--- app/core/entities/cluster_algorithm.py
+++ app/core/entities/cluster_algorithm.py
@@ -1,2 +1,95 @@
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+
 class ClusterAlgorithm:
-    pass
+
+    def dbscan(self, vectors, ep, minPts):
+        self.cluster_model = DBSCAN(eps=ep, min_samples=minPts)
+        labels = self.cluster_model.fit_predict(vectors)
+
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+
+        print("Estimated number of clusters: %d" % n_clusters_)
+        print("Estimated number of noise points: %d" % n_noise_)
+
+
+        unique_labels = set(labels)
+        core_samples_mask = np.zeros_like(labels, dtype=bool)
+        core_samples_mask[db.core_sample_indices_] = True
+
+        colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+        for k, col in zip(unique_labels, colors):
+            if k == -1:
+                # Black used for noise.
+                col = [0, 0, 0, 1]
+
+            class_member_mask = labels == k
+
+            xy = X[class_member_mask & core_samples_mask]
+            plt.plot(
+                xy[:, 0],
+                xy[:, 1],
+                "o",
+                markerfacecolor=tuple(col),
+                markeredgecolor="k",
+                markersize=14,
+            )
+
+            xy = X[class_member_mask & ~core_samples_mask]
+            plt.plot(
+                xy[:, 0],
+                xy[:, 1],
+                "o",
+                markerfacecolor=tuple(col),
+                markeredgecolor="k",
+                markersize=6,
+            )
+
+        plt.title(f"Estimated number of clusters: {n_clusters_}")
+        plt.show()
+        return labels
+
+    def test_dbscan_parameters(self, vectors, eps_values, min_samples_values, use_pca=True, n_components=2):
+        if use_pca:
+            pca = PCA(n_components=n_components)
+            reduced_vectors = pca.fit_transform(vectors)
+        else:
+            reduced_vectors = vectors
+
+        fig, axes = plt.subplots(len(eps_values), len(min_samples_values), figsize=(15, 10))
+        fig.suptitle('DBSCAN Clusters para diferentes (Eps, MinPts)', fontsize=16)
+
+        for i, eps in enumerate(eps_values):
+            for j, min_samples in enumerate(min_samples_values):
+                self.cluster_model = DBSCAN(eps=eps, min_samples=min_samples, metric='l2')
+                labels = self.cluster_model.fit_predict(vectors)
+
+                if use_pca:
+                    data_to_plot = reduced_vectors
+                else:
+                    data_to_plot = TSNE(n_components=2).fit_transform(vectors)
+
+                ax = axes[i, j]
+                unique_labels = set(labels)
+                colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+
+                for k, col in zip(unique_labels, colors):
+                    if k == -1:
+                        col = [0, 0, 0, 1]
+
+                    class_member_mask = (labels == k)
+                    xy = data_to_plot[class_member_mask]
+                    ax.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                            markeredgecolor='k', markersize=6)
+
+                ax.set_title(f'Eps={eps}, MinPts={min_samples}')
+          
+
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+        plt.show()
+
+    
--- app/core/usecases/group_candidates.py
+++ app/core/usecases/group_candidates.py
@@ -3,7 +3,9 @@ from ..interfaces.vector_store import IVectorStore
 from helpers.dict_to_json import dict_to_json
 import numpy as np
 from ..entities.vectorizer import Vectorizer
+from ..entities.cluster_algorithm import ClusterAlgorithm
 import os, json
+from helpers.visualize_similarity import *
 
 
 class GroupCandidates:
@@ -12,6 +14,7 @@ class GroupCandidates:
         self.object_store = object_store
         self.vector_store = vector_store
         self.vectorizer = Vectorizer()
+        self.cluster_algorithm = ClusterAlgorithm()
     
     def vectorize_texts(self, objects, vector_to_text_id):
         all_embeddings = np.empty((0, 768))
@@ -40,20 +43,45 @@ class GroupCandidates:
                
         if vectors_bool: 
             index, vector_to_text_id = self.vector_store.get_vectors()
+            vectors = index.reconstruct_n(0, index.ntotal)
         else: 
             objects = self.object_store.list_dl_objects(prefix="refined")
             all_embeddings, vector_to_text_id = self.vectorize_texts(objects, vector_to_text_id)
             index = self.vector_store.save_vectors(all_embeddings, vector_to_text_id)
         
-        query_vector = self.vectorizer.vectorize("Desenvolver conhecimentos sobre as propriedades das biocerâmicas para aplicações em odontologia e medicina.")
+        # query_vector = self.vectorizer.vectorize("Desenvolver conhecimentos sobre as propriedades das biocerâmicas para aplicações em odontologia e medicina.")
 
-        distances, indices = self.vector_store.search_similar(query_vector, index, k=10)
+        # distances, indices = self.vector_store.search_similar(query_vector, index, k=10)
 
-        similar_text_ids = [vector_to_text_id[str(idx)] for idx in indices[0]]
-        print(similar_text_ids)
-        
-
-        
+        # similar_text_ids = [vector_to_text_id[str(idx)] for idx in indices[0]]
+        # print(similar_text_ids)
         
 
 
+        # # !EXPERIMENTO SIMILARIDADE
+
+        # query_text = "Desenvolver conhecimentos sobre as propriedades das biocerâmicas para aplicações em odontologia e medicina."
+        # query_vector = self.vectorizer.vectorize(query_text)
+
+        # distances, indices = self.vector_store.search_similar(query_vector, index, k=15)
+
+        # similar_text_ids = [vector_to_text_id[str(idx)] for idx in indices[0]]
+        # print(similar_text_ids)
+        # print(distances)
+
+        # similar_vectors = vectors[indices[0]]
+        # # visualize_similarity_results(query_text, similar_text_ids, distances[0])
+        # # visualize_similarity_results_with_query(query_vector, similar_vectors, similar_text_ids, perplexity=min(5, len(similar_vectors)-1))
+     
+
+        #!EXPERIMENTO CLUSTERING
+        eps_values = [5, 7]
+        min_samples_values = [769, 1536]
+
+        find_optimal_eps(vectors, k=1536)
+
+        print(self.cluster_algorithm.dbscan(vectors, ep=9.3, minPts=1536))
+
+        # self.cluster_algorithm.test_dbscan_parameters(vectors, eps_values, min_samples_values, use_pca=True)
+        # self.cluster_algorithm.test_dbscan_parameters(vectors, eps_values, min_samples_values, use_pca=False)
+        
